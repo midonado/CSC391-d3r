540771d208d157e8d399eb8ac86eecf70fab5562
midonado@davidson.edu <55297780+midonado@users.noreply.github.com>
2022-02-22 22:57:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,69 +1,12 @@
 /** App.js */
 import React, { useState, useEffect } from "react";
-import BarChart from "./BarChart";
+import BarChart from "./views/BarChart/BarChart";
 import "./App.css";
-import * as d3 from "https://cdn.skypack.dev/d3@7";
-import { csv } from "https://cdn.skypack.dev/d3-fetch@3";
-import csvFile from "./data/dataFromClass.csv";
+// import csvFile from "./data/dataFromClass.csv";
+// import * as d3 from "https://cdn.skypack.dev/d3@7";
 
 export default function App() {
-  const [rawData, setRawData] = useState([]);
 
-  useEffect(() => {
-    const fetchCSV = async (dataAddress) => {
-      const csvObj = d3.dsv(",", dataAddress, (d) => {
-        return {
-          lines: +d.lines,
-          time: +d.time,
-          highlight: d.hihlight === "TRUE"
-        };
-      });
-
-      setRawData(csvObj);
-    }
-
-    fetchCSV(csvFile);
-  }, []);
-
-  console.log(rawData);
-
-  // const id = (d) => d.lines + "_" + d.highlight;
-
-  // let avgData = {}
-
-  // rawData.forEach(d => {
-  //   console.log(d);
-  // });
-
-  // rawData.forEach(d => {
-  //   const curID = id(d);
-
-  //   if (!avgData.hasOwnProperty(curID)) {
-  //     avgData[curID] = {
-  //       lines: d.lines,
-  //       times: [],
-  //       highlight: d.highlight,
-  //       label: curID
-  //     };
-  //   }
-
-  //   avgData[curID].times.push(d.time);
-  // });
-
-  // avgData.forEach(d => {
-  //   d.time = d3.mean(d.times)
-  // });
-
-  // console.log(avgData);
-
-  // avgData.sort((a, b) => {
-  //   if (a.lines === b.lines)
-  //     return d3.descending(a.highlight, b.highlight)
-  //   else
-  //     return d3.ascending(a.lines, b.lines)
-  // });
-
-  // const max = d3.max(avgData, d => d.time);
 
   const dimensions = {
     width: 960,
@@ -75,12 +18,14 @@ export default function App() {
       left: 40
     }
   }
+
+  const attr = [{ color: "red" }]
+
   return (
     <div className="App">
       <BarChart
-        data={[{ time: 5, label: "true" }, { time: 4, label: "false" }]}
+        attributes={attr}
         dimensions={dimensions}
-        max={5}
       />
     </div>
   );
