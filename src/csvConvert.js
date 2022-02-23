b5dd59520316bc9ec3197a5e586db5406ad99377
midonado@davidson.edu <55297780+midonado@users.noreply.github.com>
2022-02-22 22:57:41 -0500

    clean up

--- src/csvConvert.js
+++
@@ -1,6 +0,0 @@
-let csvToJson = require('convert-csv-to-json');
-
-let fileInputName = '../public/dataFromClass.csv'; 
-let fileOutputName = 'myOutputFile.json';
-
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
