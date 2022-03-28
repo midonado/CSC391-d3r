8b057e1f22f159b51fdad13feba234802b299dbf
midonado@davidson.edu <55297780+midonado@users.noreply.github.com>
2022-03-28 12:35:44 -0400

    layout and more buttons

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   // going through BarChart, allowing it to render properly. Potentially due to npm start
   const [preLoad, setPreLoad] = useState([]);
 
-  const preLoadData = async (dataAddress) => {
+  const preLoadData = async () => {
     setPreLoad([]);
   }
 
@@ -16,23 +16,13 @@ export default function App() {
     preLoadData();
   }, [])
 
-  // const data = [
-  //   { highlight: true, label: '1_TRUE', time: 3.477368421052631 },
-  //   { highlight: false, label: '4_FALSE', time: 36.59315789473684 },
-  //   { highlight: false, label: '2_FALSE', time: 31.410526315789472 },
-  //   { highlight: false, label: '3_FALSE', time: 22.92684210526316 },
-  //   { highlight: true, label: '4_TRUE', time: 3.3436842105263156 },
-  //   { highlight: false, label: '1_FALSE', time: 6.936842105263159 },
-  //   { highlight: true, label: '2_TRUE', time: 1.7405263157894737 },
-  //   { highlight: true, label: '3_TRUE', time: 2.3168421052631576 }];
-
   const fruitData = [
     { fruit: "Banana", count: 40, color: "yellow" },
     { fruit: "Strawberry", count: 32, color: "red" },
     { fruit: "Blueberry", count: 18, color: "blue" },
     { fruit: "Mango", count: 56, color: "orange" },
     { fruit: "Pineapple", count: 45, color: "green" },
-    { fruit: "Grape", count: 40, color: "purple" }];
+    { fruit: "Grape", count: 40, color: "rebeccapurple" }];
 
   const dimensions = {
     width: 960,
@@ -40,8 +30,8 @@ export default function App() {
     margin: {
       top: 20,
       right: 40,
-      bottom: 35,
-      left: 40
+      bottom: 60,
+      left: 60
     }
   }
 
--- src/views/BarChart/BarChart.js
+++ src/views/BarChart/BarChart.js
@@ -6,24 +6,40 @@ const BarChart = ({ data = [], dimensions = {} }) => {
     const [flag, setFlag] = useState(true);
     const [attr, setAttr] = useState({
         bgColorToggle: false, bgColor: "#aaa",
-        barColorToggle: true, barColorDefault: "#333",
+        bgBorderToggle: false, borderColor: "#000",
+        barColorToggle: true, barColorDefault: "#222",
+        barBorderToggle: false,
+        showAxesToggle: true,
+        showAxisTitleToggle: true,
+        showAxisLableToggle: true,
+        titleToggle: true, subtitleToggle: true
     });
     const svgRef = useRef(null);
-    const { width, height, margin = {} } = dimensions;
+    var { width, height, margin = {} } = dimensions;
     const svgWidth = width + margin.left + margin.right;
     const svgHeight = height + margin.top + margin.bottom;
 
     const svgEl = d3.select(svgRef.current)
     svgEl.selectAll("*").remove(); // Clear svg content before adding new elements
 
+    // Update Flag, Re-render Chart
     const updateArray = () => {
         setFlag(!flag);
     }
 
+    // Toggle Handle Functions
+    //TODO: make less repetitive, pass attribute being toggled as prop?
     const handleBgColorClick = () => {
         updateArray();
         const temp = attr;
         temp.bgColorToggle = !temp.bgColorToggle;
+        setAttr(temp);
+    }
+
+    const handleBgBorderClick = () => {
+        updateArray();
+        const temp = attr;
+        temp.bgBorderToggle = !temp.bgBorderToggle;
         setAttr(temp);
     }
 
@@ -34,7 +50,78 @@ const BarChart = ({ data = [], dimensions = {} }) => {
         setAttr(temp);
     }
 
-    const drawEntries = (svg, x, y) => {
+    const handleBarBorderClick = () => {
+        updateArray();
+        const temp = attr;
+        temp.barBorderToggle = !temp.barBorderToggle;
+        setAttr(temp);
+    }
+
+    const handleShowAxesClick = () => {
+        updateArray();
+        const temp = attr;
+        temp.showAxesToggle = !temp.showAxesToggle;
+        setAttr(temp);
+    }
+
+    const handleShowAxisTitleClick = () => {
+        updateArray();
+        const temp = attr;
+        temp.showAxisTitleToggle = !temp.showAxisTitleToggle;
+        setAttr(temp);
+    }
+
+    const handleShowAxisLablesClick = () => {
+        updateArray();
+        const temp = attr;
+        temp.showAxisLableToggle = !temp.showAxisLableToggle;
+        setAttr(temp);
+    }
+
+    // Draw Chart Functions
+
+    const createAxes = (max) => {
+        const x = d3.scaleBand()
+            .rangeRound([0, width])
+            .padding(0.1)
+            .domain(data.map(d => d.fruit).sort());
+
+        var y = d3.scaleLinear()
+            .rangeRound([height, 0])
+            .domain([0, max]);
+
+        return { x, y }
+    }
+
+    const drawAxes = (svg, x, y) => {
+        svg.append("g")
+            .attr("transform", "translate(0," + (height) + ")")
+            .call(d3.axisBottom(x))
+
+        svg.append("g")
+            .call(d3.axisLeft(y))
+    }
+
+    const drawAxisTitle = (svg, x, y) => {
+        // Horizontal Axis Label
+        svg.append("text")
+            .attr("class", "x-label")
+            .attr("text-anchor", "middle")
+            .attr("x", width / 2)
+            .attr("y", height + margin.bottom * 0.65)
+            .text(x);
+
+        // Vertical Axis Label
+        svg.append("text")
+            .attr("class", "y-label")
+            .attr("text-anchor", "middle")
+            .attr("transform", "rotate(-90)")
+            .attr("x", -height / 2)
+            .attr("y", -margin.left * 0.55)
+            .text(y);
+    }
+
+    const drawEntries2D = (svg, x, y) => {
         const entries = svg.selectAll("g")
             .data(data)
             .enter()
@@ -48,50 +135,84 @@ const BarChart = ({ data = [], dimensions = {} }) => {
                 .attr("y", y(d.count))
                 .attr("height", height - y(d.count))
                 .attr("width", x.bandwidth())
-                .style("fill", attr.barColorToggle ? d => d.color : attr.barColorDefault)
-                .attr("stroke", "black")
+                .style("fill", d => d.color)
         })
     }
 
     useEffect(() => {
         const svg = svgEl
             .style("background", attr.bgColorToggle ? attr.bgColor : "#fff")
+            .attr("class", attr.bgBorderToggle ? "bar-chart--border" : "bar-chart")
             .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
         const max = d3.max(data, d => d.count);
 
-        const x = d3.scaleBand()
-            .rangeRound([0, width])
-            .padding(0.1)
-            .domain(data.map(d => d.fruit).sort());
+        const { x, y } = createAxes(max);
 
-        const y = d3.scaleLinear()
-            .rangeRound([height, 0])
-            .domain([0, max]);
+        drawEntries2D(svg, x, y);
 
-        drawEntries(svg, x, y);
+        if (!attr.barColorToggle)
+            svg.selectAll(".bar").style("fill", attr.barColorDefault)
 
-        svg.append("g")
-            .attr("transform", "translate(0," + (height) + ")")
-            .call(d3.axisBottom(x))
 
-        svg.append("g")
-            .call(d3.axisLeft(y))
+        if (attr.barBorderToggle) {
+            svg.selectAll(".bar")
+                .style("stroke", attr.borderColor)
+        }
+
+        if (attr.showAxesToggle)
+            drawAxes(svg, x, y);
+
+
+        if (attr.showAxisTitleToggle)
+            drawAxisTitle(svg, "Fruits", "Votes");
+
+
+        if (!attr.showAxisLableToggle)
+            svg.selectAll(".tick").selectAll("text").attr("fill", "transparent")
+
+        //TODO: include title + subtitle in useEffect
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [data, flag]);
 
     return (<>
-        <svg ref={svgRef} width={svgWidth} height={svgHeight} />
-
         <div className="toggles">
             <button onClick={handleBgColorClick} >
                 Background Color
             </button>
+            <button onClick={handleBgBorderClick} >
+                Background Border
+            </button>
             <button onClick={handleBarColorClick} >
                 Bar Color
             </button>
+            <button onClick={handleBarBorderClick} >
+                Bar Borders
+            </button>
+            <button onClick={handleShowAxesClick} >
+                Show Axes
+            </button>
+            <button onClick={handleShowAxisTitleClick} >
+                Show Axis Titles
+            </button>
+            <button onClick={handleShowAxisLablesClick} >
+                Show Axis Data Lables
+            </button>
+            {/* <button onClick={handleShowTitleClick} >
+                Show Chart Title
+            </button>
+            <button onClick={handleShowSubtitleClick} >
+                Show Chart Subitles
+            </button> */}
+        </div>
+        <div className="chart-container">
+            {/* <div>
+                {attr.titleToggle && <h1>Favorite Fruit Survey</h1>}
+                {attr.subtitleToggle && <h3>hello</h3>}
+            </div> */}
+            <svg ref={svgRef} width={svgWidth} height={svgHeight} />
         </div>
     </>);
 };
